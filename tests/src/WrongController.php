3eee29287b6eb890bceca969fd2ca964433046c5
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2025-02-22 12:46:01 -0300

    typo

--- tests/src/WrongController.php
+++ tests/src/WrongController.php
@@ -22,11 +22,13 @@ use Chevere\Http\Status;
 use Chevere\Parameter\Interfaces\ArgumentsInterface;
 use Chevere\Parameter\Interfaces\ArrayParameterInterface;
 use Chevere\Parameter\Interfaces\ArrayStringParameterInterface;
+use Chevere\Parameter\Interfaces\CastInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use function Chevere\Parameter\arguments;
 use function Chevere\Parameter\arrayp;
 use function Chevere\Parameter\arrayString;
+use function Chevere\Parameter\cast;
 
 final class WrongController extends Action implements ControllerInterface
 {
@@ -87,6 +89,11 @@ final class WrongController extends Action implements ControllerInterface
     public function attributes(): MapInterface
     {
         return new Map();
+    }
+
+    public function attribute(string $name, mixed $default = null): CastInterface
+    {
+        return cast(null);
     }
 
     public function status(): StatusInterface
