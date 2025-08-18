0e1f9b5f40b80c8e55c4c3d0c7d2b951b2311657
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2025-08-18 14:15:58 -0400

    add method to 404 error

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -46,7 +46,8 @@ final class Dispatcher implements DispatcherInterface
             GroupCountBased::FOUND => new Dispatch($format[1], $format[2]),
             GroupCountBased::NOT_FOUND => throw new NotFoundException(
                 (string) message(
-                    'No route found for `%uri%`',
+                    'No routing found for %method% `%uri%`',
+                    method: $method,
                     uri: $uri,
                 ),
                 404
