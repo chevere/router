c8c70161ec88f8394b54f89749370f54e9a0e268
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2025-09-08 15:34:28 -0300

    typo

--- src/functions.php
+++ src/functions.php
@@ -69,7 +69,7 @@ function getPath(string $path, string|BindInterface ...$bind): string
         } catch (Throwable) {
             continue;
         }
-        $controllerName::newReflection();
+        $controllerName::reflection();
         foreach ($routePath->variables()->keys() as $variable) {
             $variableBracket = <<<STRING
             {{$variable}}
